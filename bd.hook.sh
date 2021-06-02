880bdda38d781f1e6c47e380b8e277adc0c5dd45
AXON <axonasif@gmail.com>
2021-06-02 00:27:41 +0000

    chore: Initial commit

--- bd.hook.sh
+++ bd.hook.sh
@@ -6,22 +6,9 @@ BT="$TMP_DIR/add_custom_ghome"
 if test -e "$BT"; then
 	rm -rf "$BT"
 fi
+
 mkdir -p "$BT"
-
 gclone "$HOOK_BASE/ghome" "$BT"
-
-# Handle enc
-readarray -d '' encrypted_files < <(find "$BT" -name "*.enc" -print0)
-read -p 'Encrypt add_custom_ghome? [y/n]' ENC
-if test "${ENC,,}" == "n"; then
-	for f in "${encrypted_files[@]}"; do
-		rm "$f"
-	done
-else
-	for f in "${encrypted_files[@]}"; do
-		mv "$f" "${f%.enc}"
-	done
-fi
 
 # Package assets
 cd "$BT/ghome/dependencies/assets"
@@ -35,5 +22,6 @@ cd "$GBDIR/boot-comp"
 test "$DISTRO_NAME" == "PrimeOS" && {
 	mv rusty-panda_$HOST_ARCH rusty-panda && rm rusty-panda_*
 }
+
 rm -r "$BT"
 chown root:root -hR "$GHOME"
